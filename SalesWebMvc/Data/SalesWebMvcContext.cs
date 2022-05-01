4cbff4cc7228ad86908acd22215af87463b7358b
Danielle Santiago <dlaurindo@indracompany.com>
2022-05-01 17:29:59 -0300

    Conexão com banco de dados

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -1,10 +1,6 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Data
 {
